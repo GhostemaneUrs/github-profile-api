3962586e817cc00beebdbd7b52042c673f7dffaa
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-25 18:20:08 -0500

    feat(root): implements profileCard in searchApp

--- src/pages/SearchApp.jsx
+++ src/pages/SearchApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
+import ProfileCard from "../components/ProfileCard";
 
 const SearchApp = () => {
     return (
@@ -8,6 +9,7 @@ const SearchApp = () => {
             <div className="container">
                 <Header title="GitHub Profile Search" />
                 <SearchBar />
+                <ProfileCard />
             </div>
         </>
     );
