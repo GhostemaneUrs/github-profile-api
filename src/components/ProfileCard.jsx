2756f8cbb6ccdada81c6ed6f0c99800766e47caa
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-25 21:54:52 -0500

    feat(root): profileCard

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-
-
-
-const ProfileCard = () => {
+import { useNavigate } from 'react-router';
+const ProfileCard = ({ user }) => {
+    const navigate = useNavigate();
     return (
         <>
-            <div className="profile-container">
-                <div className="profile-content">
+            <div className="profile-container" onClick={() =>
+                navigate("/DetailsUserApp", {
+                    user: user,
+                })
+            }
+            >
+                <div className="profile-content" style={{ "width": "100%" }} >
                     <div className="profile-header">
-                        <img className="avatar" src="https://avatars.githubusercontent.com/u/583231?v=4" alt="avatar-user" />
-                        <div className="profile-info">
-                            <div className="profile-name">
-                                <h2 className="name">The Octocat</h2>
-                                <p className="user">@octocat</p>
-                                <p className="bio">Not descriptions</p>
-                            </div>
+                        <img className="avatar" src={user?.avatar_url ? user?.avatar_url : "https://avatars.githubusercontent.com/u/583231?v=4"} alt="avatar-user" />
+                        <div className="profile-info profile-name">
+
+                            <h2 className="name text-ellipsis">{user?.name ? user?.name : user.login}</h2>
+                            <p className="user text-ellipsis">{user.login}</p>
+                            <p className="bio text-ellipsis">{user?.bio ? user?.bio : 'Not descriptions'}</p>
+
                         </div>
                     </div>
                 </div>
