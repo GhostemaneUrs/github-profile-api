36e1ce424dca5dbf2798ad43aa1ce50b36d8faa8
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-25 21:55:38 -0500

    feat(root): searchBard component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getGitHubProfile } from "../actions"
+import { useDispatch, useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
+import ProfileCard from './ProfileCard';
+
 
 const SearchBar = () => {
+    const [search, setSearch] = useState("");
+    const dispatch = useDispatch();
+    const { usuarios } = useSelector(state => state.gitHub);
+    console.log(usuarios);
+    const buscar = () => {
+        if (search.trim() === "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Debes ingresar un nombre de usuario',
+            })
+        } else {
+            dispatch(getGitHubProfile(search));
+        }
+    }
     return (
-        <div className="flex justify-between items-center container-searchbar active">
-            <label className="flex-1" for="input">
-                <input type="search" name="user-input" id="inputSearch" className="inputSearch" placeholder="Search Github username.." />
-            </label>
-            <button className="btn-search" id="submit">Search</button>
-        </div>
+        <>
+            <div className="flex justify-between items-center container-searchbar active">
+                <label className="flex-1" for="input">
+                    <input type="search" name="user-input" id="inputSearch" className="inputSearch" placeholder="Search Github username.." onChange={(e) => setSearch(e.target.value)} />
+                </label>
+                <button className="btn-search" onClick={buscar}>Search</button>
+            </div>
+            <div className="grid gap-3 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+                {usuarios.map((user) => (
+                    <ProfileCard key={user?.id} user={user} />
+                ))}
+            </div>
+        </>
     );
 }
 
